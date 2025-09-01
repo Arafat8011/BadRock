9a9a2a61e23228431e9b2c8d9808da6d3d7cdd8e
MD ARAFAT HOSEN <115610256+Arafat8011@users.noreply.github.com>
2025-09-01 05:48:48 +0000

    fix: card hover state

--- src/app/products/item/[id]/page.tsx
+++ src/app/products/item/[id]/page.tsx
@@ -4,7 +4,7 @@ import products from "../../../../../db.json";
 import { ProductTypes } from "@/types";
 
 export default function Page({ params }: { params: { id: string } }) {
-  const product = products.find((p) => p.id === params.id) as ProductTypes | undefined;
+  const product = products.find((p) => p?.id === params?.id) as ProductTypes | undefined;
   console.log(product);
 
   if (!product) {
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,12 +14,12 @@ export default function ProductCard({ product }: { product: ProductTypes }) {
         <p className='text-color-muted'>{product.brand}</p>
         <p className="text-color-accent">${product.price}</p>
         <div className="flex gap-2 mt-2">
-          <button 
+          <button
             onClick={() => handleAddToCart(product)}
             className="w-full py-2 rounded  bg-color-primary text-color-primary-fg cursor-pointer  hover:bg-color-button-hover active:bg-color-button-active  transition">
             Add to Cart
           </button>
-          <Link href={`/products/item/${product.id}`} className="w-full py-2 rounded border border-color-primary text-center  text-color-primary cursor-pointer  hover:bg-color-button-hover active:bg-color-button-active  transition">
+          <Link href={`/products/item/${product.id}`} className="w-full py-2 rounded border border-color-primary text-center  text-color-primary  cursor-pointer hover:text-color-primary-fg  hover:bg-color-button-hover  active:bg-color-button-active  transition">
             View Details
           </Link>
         </div>
